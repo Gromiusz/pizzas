b4aa27bd0f2dccbf26d7f543e2e1184f5a367969
Kacper Górka <gromiusz@MacBook-Air-Kacper.local>
2024-07-24 11:40:46 +0200

    gmock not working

--- test/PizzeriaTest.cpp
+++ test/PizzeriaTest.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
+#include "gmock/gmock.h"
 #include <string>
-#include "mocks/PizzaMock.hpp"
+//#include "mocks/PizzaMock.hpp"
 #include "Pizzeria.hpp"
 #include "Margherita.hpp"
 #include "Funghi.hpp"
@@ -12,6 +13,50 @@ struct PizzeriaTest : public ::testing::Test
 {
 public:
     Pizzeria pizzeria = Pizzeria("dummyName"); 
+};
+
+class PizzaDummy : public Pizza 
+{
+public:
+    PizzaDummy() : Pizza("DummyPizza", 10.0, minutes(0)) {}
+    std::string getName() const override {return "Dummy";}
+    double getPrice() const override {return 10;}
+    minutes getBakingTime() const override {return minutes(0);}
+};
+
+class PizzaStub : public Pizza
+{
+public:
+    PizzaStub(std::string const & name) : Pizza(name, 10.0, minutes(1)) {}
+    std::string getName() const override {return Pizza::getName();}
+    double getPrice() const override 
+    {
+        std::string name = Pizza::getName();
+        if(name.size() >= 4)
+        {
+            return static_cast<double>(name[3]);
+        }
+        return 10.0;
+    }
+    minutes getBakingTime() const override 
+    {
+        double sum = 0;
+        std::string name = Pizza::getName();
+        for(const auto & letter : name)
+        {
+            sum += (static_cast<double>(letter)/100);
+        }
+       return minutes(static_cast<long int>(sum));
+    }
+};
+
+class PizzaMock : public Pizza
+{
+public:
+    PizzaMock() : Pizza("PizzaMock", 5.0, minutes(1)) {}
+    MOCK_METHOD(std::string, getName, (), (const, override));
+    MOCK_METHOD(double, getPrice, (), (const, override));
+    MOCK_METHOD(minutes, getBakingTime, (), (const, override));
 };
 
 
